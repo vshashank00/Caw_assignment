a453de8ab131d834252948536926ab5b1c3eca61
vshashank00 <51900380+vshashank00@users.noreply.github.com>
2024-02-28 22:24:13 +0530

    Update Indexpage.java

--- src/main/java/org/example/Indexpage.java
+++ src/main/java/org/example/Indexpage.java
@@ -39,7 +39,7 @@ public class Indexpage {
        for (int i=0;i<list.size();i++){
            String arr[]=list.get(i).getText().split(" ");
            Assert.assertTrue(arr[0].equalsIgnoreCase(hashMapList.get(i).get("name")) && arr[1].equalsIgnoreCase(hashMapList.get(i).get("age")) && arr[2].equalsIgnoreCase(hashMapList.get(i).get("gender")));
-           System.out.println("verifiocation done for row:" + i + " " + hashMapList.get(i));
+           System.out.println("verifiocation done for row:" + (i+1) + " " + hashMapList.get(i));
 
        }
     }
