e358a710a0b3bd5dffecba678c7e91eb073eb3f7
vshashank00 <51900380+vshashank00@users.noreply.github.com>
2024-02-28 16:32:49 +0530

    Update Checkdata.java
    final

--- src/test/java/org/example/Checkdata.java
+++ src/test/java/org/example/Checkdata.java
@@ -11,10 +11,11 @@ public class Checkdata extends BaseTest {
 
     @Test(enabled = true)
     void jsontoString() throws IOException {
-        Feed_data feed_data = new Feed_data();
-        List<HashMap<String, String>> hashMapList = feed_data.extractdata();
-        indexpage.feedData(Feed_data.jsonData);
-        indexpage.verify_feed_data(hashMapList);
+        Feed_data feed_data = new Feed_data();//extraction of data is perform in this class.
+        List<HashMap<String, String>> hashMapList = feed_data.extractdata();//transferring json into a list hashmap.
+        indexpage.feedData(Feed_data.jsonData);//feedData method fill feed the data in text box.
+        indexpage.verify_feed_data(hashMapList);//verification will be done between feeded data and the data the data present in jsndata file.
+
 
 
     }
